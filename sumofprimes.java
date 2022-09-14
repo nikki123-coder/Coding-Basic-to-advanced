b23af300072d2357477ee733fc3debfe3fc2248b
nikki123-coder <60722664+nikki123-coder@users.noreply.github.com>
2022-09-14 22:31:40 +0530

    Initial commit

---
+++ sumofprimes.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            long ans = ob.prime_Sum(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    public long prime_Sum(int n)
+    {
+        // code here
+        long sum =0;
+        if(n<=1){
+            return sum;
+        }
+        if(n==2){
+            return 2;
+        }
+        for(int i=2; i<=n; i++){
+            
+            sum = sum+sum(i);
+        }
+        return sum;
+        
+    }
+    int sum(int m){
+        for(int i=2; i<=Math.sqrt(m); i++){
+            if(m%i==0){
+            return 0;
+        }
+        }
+        return m;
+    }
+    
+}
